5348047e32bd6d5ec8900badb76bef29e07c6e25
Dewiie0 <111650051+Dewiie0@users.noreply.github.com>
2023-01-20 15:46:03 +0100

    Movement added

--- SlidingCube/Assets/Movement.cs
+++ SlidingCube/Assets/Movement.cs
@@ -6,14 +6,22 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] float speed;
     Rigidbody rb;
+    float mouseX;
 
-    private void Start()
+    void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    void Update()
     {
-        rb.AddForce(new Vector3(speed, 0, 0));
+        mouseX += Input.GetAxis("Mouse X");
+
+        if (Input.GetKey(KeyCode.W))
+        {
+           rb.AddForce(transform.forward * speed);
+        }
+
+        transform.eulerAngles = new Vector3(0, mouseX, 0);
     }
 }
